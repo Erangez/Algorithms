53154615bc37cba5e57729ce8d2e62e94b924ed8
Erangez <112576544+Erangez@users.noreply.github.com>
2025-04-01 03:25:39 +0300

    Вся работа.
    Отчёт не сделан.
    Остальное всё готово.

---
+++ Lab No. 1 type.cpp
@@ -0,0 +1,173 @@
+﻿#include <iostream>
+#include <cstring>
+#include <Windows.h>
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    //// #4
+    // Создание типизированного файла с буквами.
+    cout << "Задание №4\n";
+    FILE* fw4;
+    char letter[2];
+    char f4_name[20];
+    cout << "Введите имя файла: ";
+    cin >> f4_name;
+    fw4 = fopen(f4_name, "wb");
+    if (!fw4) {
+        cout << "Ошибка при создании файла.\n";
+        return -1;
+    }
+
+    cout << "Введите букву: ";
+    cin >> letter;
+    while (letter[0] != '0') {
+        fwrite(letter, sizeof(char), 2, fw4);
+        cout << "Введите ещё букву (для отмены введите 0): ";
+        cin >> letter;
+    }
+    cout << "Запись завершена.\n";
+    fclose(fw4);
+
+    // Решение задачи
+    FILE* fr4;
+    fr4 = fopen(f4_name, "rb");
+    if (!fr4) {
+        cout << "Ошибка при чтении файла.\n";
+        return -1;
+    }
+    string word = "";
+    while (!feof(fr4)) {
+        if (fread(letter, sizeof(char), 2, fr4) == 2) {
+            word += letter;
+        }
+    }
+    cout << "Полученное слово: " << word << "\n";
+    fclose(fr4);
+
+    //// #5
+    // Создание типизированного файла с числами.
+    cout << "\nЗадание №5\n";
+    FILE* fw5;
+    char number[30];
+    char f5_name[20];
+    cout << "Введите имя файла: ";
+    cin >> f5_name;
+    fw5 = fopen(f5_name, "wb");
+    if (!fw5) {
+        cout << "Ошибка при создании файла.\n";
+        return -1;
+    }
+    cout << "Введите число: ";
+    cin >> number;
+    while (number[0] != 'x') {
+        fwrite(number, sizeof(char), 30, fw5);
+        cout << "Введите ещё число (для отмены введите x): ";
+        cin >> number;
+    }
+    cout << "Запись завершена.\n";
+    fclose(fw5);
+
+    // Решение задачи
+    FILE* fr5;
+    fr5 = fopen(f5_name, "rb");
+    if (!fr5) {
+        cout << "Ошибка при чтении файла.\n";
+        return -1;
+    }
+    int n_max = INT32_MIN;
+    while (!feof(fr5)) {
+        fread(number, sizeof(char), 30, fr5);
+        n_max = atoi(number) > n_max ? atoi(number) : n_max;
+    }
+    cout << "Максимальное число: " << n_max << "\n";
+    fclose(fr5);
+
+    //// #6
+    // Создание типизированных файлов с числами.
+    FILE* fw6, * fw62;
+    char nums[30];
+    char f6_name1[20];
+    char f6_name2[20];
+    cout << "Введите имя файла №1: ";
+    cin >> f6_name1;
+    fw6 = fopen(f6_name1, "wb");
+    if (!fw6) {
+        cout << "Ошибка при создании файла №1.\n";
+        return -1;
+    }
+    cout << "Введите имя файла №2: ";
+    cin >> f6_name2;
+    fw62 = fopen(f6_name2, "wb");
+    if (!fw62) {
+        cout << "Ошибка при создании файла №2.\n";
+        return -1;
+    }
+    // f №1
+    cout << "Числа файла " << f6_name1 << ":\n";
+    cout << "Введите число: ";
+    cin >> nums;
+    while (nums[0] != 'x') {
+        fwrite(nums, sizeof(char), 30, fw6);
+        cout << "Введите ещё число (для отмены введите x): ";
+        cin >> nums;
+    }
+    cout << "Запись в файл " << f6_name1 << " завершена.\n";
+    fclose(fw6);
+
+    // f №2
+    cout << "Числа файла " << f6_name2 << ":\n";
+    cout << "Введите число: ";
+    cin >> nums;
+    while (nums[0] != 'x') {
+        fwrite(nums, sizeof(char), 30, fw62);
+        cout << "Введите ещё число (для отмены введите x): ";
+        cin >> nums;
+    }
+    cout << "Запись в файл " << f6_name2 << " завершена.\n";
+    fclose(fw62);
+
+    // Решение задачи
+    FILE* fw63, *fr6, *fr62;
+    char f6_name3[20];
+    cout << "Введите имя файла для записи: ";
+    cin >> f6_name3;
+    fw63 = fopen(f6_name3, "wb");
+    if (!fw63) {
+        cout << "Ошибка при создании файла " << f6_name3 << ".\n";
+        return -1;
+    }
+    fr6 = fopen(f6_name1, "rb");
+    if (!fr6) {
+        cout << "Ошибка при чтении файла " << f6_name1 << ".\n";
+        return -1;
+    }
+    fr62 = fopen(f6_name2, "rb");
+    if (!fr62) {
+        cout << "Ошибка при чтении файла " << f6_name2 << ".\n";
+        return -1;
+    }
+    while (!feof(fr6)) {
+        fread(nums, sizeof(char), 30, fr6);
+        fwrite(nums, sizeof(char), 30, fw63);
+    }
+    while (!feof(fr62)) {
+        fread(nums, sizeof(char), 30, fr62);
+        fwrite(nums, sizeof(char), 30, fw63);
+    }
+    cout << "Запись закончена.\n";
+    fclose(fw63);
+    fclose(fr6);
+    fclose(fr62);
+
+    cout << "Вывод чисел из созданного файла " << f6_name3 << ":\n";
+    FILE* fr63;
+    fr63 = fopen(f6_name3, "rb");
+    while (!feof(fr63)) {
+        fread(nums, sizeof(char), 30, fr63);
+        cout << nums << " ";
+    }
+    return 0;
+
+}
---
+++ Lab. No. 1 text.cpp
@@ -0,0 +1,113 @@
+﻿#include <iostream>
+#include <cstring>
+#include <Windows.h>
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    FILE* fp1;
+    fp1 = fopen("files/info.txt", "r");
+    if (!fp1) {
+        cout << "Файл открыть не удалось.\n";
+        return -1;
+    }
+    char str1[50];
+    cout << "Содержимое файла info.txt: \n";
+    while (!feof(fp1)) {
+        if (fgets(str1, 50, fp1) != NULL) {
+            cout << str1;
+        }
+        else {
+            cout << "Ошибка при чтении файла. \n";
+            return -1;
+        }
+    }
+
+    //// #1
+    cout << "\n\nЗадание №1";
+    rewind(fp1);
+    cout << "\nВывод первых пяти строк: \n";
+    int counter = 0;
+    while (!feof(fp1)) {
+        if (fgets(str1, 50, fp1) != NULL) {
+            if (counter > 4) { break; }
+            cout << str1;
+            counter++;
+        }
+        else {
+            cout << "Ошибка при чтении файла. \n";
+            return -1;
+        }
+    }
+
+    //// #2
+    cout << "\nЗадание №2\n";
+    cout << "Количество строк, начинающихся с букв 'а' или 'А' в файле info.txt\n";
+    rewind(fp1);
+    int amount_of_str = 0;
+    while (!feof(fp1)) {
+        if (fgets(str1, 50, fp1) != NULL) {
+            if (str1[0] == 'А' || str1[0] == 'а') {
+                cout << str1;
+            }
+        }
+        else {
+            cout << "Ошибка при чтении файла. \n";
+            return -1;
+        }
+    }
+    fclose(fp1);
+
+    //// #3
+    cout << "\n\nЗадание №3\n";
+    cout << "Дан текстовый файл:\n";
+    FILE* fp3, * fp3_out;
+    char str3[50];
+    fp3 = fopen("files/info2.txt", "r");
+    if (!fp3) {
+        cout << "Файл не удалость открыть в режиме чтения.\n";
+        return -1;
+    }
+    cout << "Содержимое файла info2.txt:\n";
+    while (!feof(fp3)) {
+        if (fgets(str3, 50, fp3)) {
+            cout << str3;
+        }
+    }
+
+    fp3_out = fopen("files/info2_out.txt", "w");
+    char str3_out[50];
+    if (!fp3_out) {
+        cout << "Файл не удалость открыть в режиме записи.\n";
+        return -1;
+    }
+
+    cout << "\nВведите файлы ch1 и ch2 для замены: \n";
+    char ch1, ch2;
+    cin >> ch1;
+    cin >> ch2;
+
+    rewind(fp3);
+    while (!feof(fp3)) {
+        if (fgets(str3, 50, fp3)) {
+            for (int i = 0; i < 50; i++) {
+                if (str3[i] == ch1) { str3_out[i] = ch2; }
+                else if (str3[i] == ch2) { str3_out[i] = ch1; }
+                else { str3_out[i] = str3[i]; }
+            }
+            if (fputs(str3_out, fp3_out) == EOF) {
+                cout << "Ошибка при записи в файл.\n";
+                return -1;
+            }
+        }
+        else {
+            cout << "Ошибка при чтении файла. \n";
+            return -1;
+        }
+    }
+    cout << "Замена ch1 на ch2 и наоборот завершена.\n";
+    fclose(fp3);
+    fclose(fp3_out);
+    return 0;
+}
