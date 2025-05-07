a8faec7e16ba42cf57df759353e1b430f0cee091
Erangez <112576544+Erangez@users.noreply.github.com>
2025-05-08 01:41:25 +0300

    Лабораторная работа №3. Классы в C++
    • Добавление нового метода для расчёта цены по типу КПП и пробегу
    • Добавление нового экземпляра класса «Car»
    • Добавлена демонстрация всех методов для каждого объекта
    • Удалён метод «Car::Show()» в угоду показа методов-геттеров
    • Оптимизация кода

--- Lab No. 3 class.cpp
+++ Lab No. 3 class.cpp
@@ -27,10 +27,10 @@ public:
 	Car(int year_of_release, int price, int mileage, string brand, string vin, string color, string body_type, string gearbox);
 	~Car();
 	// Методы
-	void show() const;
 	void addMileage(int km);
 	void changePrice(int new_price);
 	void calculatePriceBasedOnMileage();
+	void calculatePriceBasedOnGearbox();
 	// Методы доступа к закрытым данным-членам
 	int getYear() const { return year_of_release; } // const после метода = константный метод (int const ...) = константное возвращ. значение
 	int getPrice() const { return price; }
@@ -47,26 +47,16 @@ Car::Car(int year_of_release, int price, int mileage, string brand, string vin, 
 	cout << this->brand << " " << "[" << this->vin << "] " << this->year_of_release << " года была привезена в салон.\n";
 }
 Car::~Car() {
-	cout << this->brand << " " << "[" << this->vin << "] " << this->year_of_release << " года была куплена за " << price << " рублей.\n";
-}
-void Car::show() const {
-	cout << "\n------------------------------\n";
-	cout << "            ИНФО\n";
-	cout << "Бренд: " << brand << "\n";	
-	cout << "VIN: " << vin << "\n";	
-	cout << "Год: " << year_of_release << "\n";
-	cout << "Цвет: " << color << "\n";	
-	cout << "Тип кузова: " << body_type << "\n";	
-	cout << "Коробка: " << gearbox << "\n";	
-	cout << "Пробег: " << mileage << " км\n";	
-	cout << "Цена: " << price << " рублей\n";	
-	cout << "------------------------------\n\n";
+	cout << this->brand << " " << "[" << this->vin << "] " << this->year_of_release << " года была продана за " << price << " рублей.\n";
 }
 void Car::addMileage(int km) {
 	if (km > 0) {
 		mileage += km;
 		cout << "Пробег изменён с " << mileage - km << " км до " << mileage <<
 			" км для " << brand << " " << "[" << vin << "] " << year_of_release << " года.\n";
+	}
+	else if (km == 0) {
+		cout << "Пробег не изменился.\n";
 	}
 	else {
 		cout << "Невозможно изменить пробег.\n";
@@ -76,12 +66,12 @@ void Car::addMileage(int km) {
 void Car::changePrice(int new_price) {
 	if (new_price != price) {
 		price = new_price;
-		cout << "Для " << getBrand() << " " << "[" << vin << "] " << year_of_release << " года была изменена цена.\n";
-		cout << "Новая цена: " << price << "рублей.\n";
+		cout << "Для " << brand << " " << "[" << vin << "] " << year_of_release << " года была изменена цена.\n";
+		cout << "Новая цена: " << price << " рублей.\n";
 	}
 	else {
 		cout << "Невозможно изменить цену.\n";
-		cout << "Вы ввели существующую цену.\n";
+		cout << "Вы ввели предыдущую цену.\n";
 	}
 }
 void Car::calculatePriceBasedOnMileage() {
@@ -104,23 +94,81 @@ void Car::calculatePriceBasedOnMileage() {
 		cout << "Расчитанная цена: " << penalted_price << " рублей.\n";
 		price -= penalty;
 	}
-
-	
+}
+void Car::calculatePriceBasedOnGearbox() {
+	if (gearbox == "Механическая") {
+		if (mileage > 135000) {
+			price *= 0.70;
+			cout << "С учётом пробега цена снижена на 30%.\n";
+			cout << "Новая цена: " << price << " рублей\n";
+		}
+		else if (mileage < 135000 && mileage > 100000) {
+			price *= 0.85;
+			cout << "С учётом пробега цена снижена на 15%.\n";
+			cout << "Новая цена: " << price << " рублей\n";
+		}
+		else {
+			price *= 0.9;
+			cout << "Цена снижена на 5% из-за типа коробки.\n";
+			cout << "Новая цена: " << price << " рублей\n";
+		}
+	}
+	else if (gearbox == "Автомат") {
+		price *= 1.05;
+		if (mileage > 125000) {
+			price *= 0.75;
+			cout << "С учётом пробега цена снижена на 25%.\n";
+			cout << "Новая цена: " << price << " рублей\n";
+		}
+		else if (mileage < 125000 && mileage > 100000) {
+			price *= 0.80;
+			cout << "С учётом пробега цена снижена на 20%.\n";
+			cout << "Новая цена: " << price << " рублей\n";
+		}
+		else {
+			cout << "Цена увеличилась на 5% из-за типа коробки.\n";
+			cout << "Новая цена: " << price << " рублей\n";
+		}
+	}
+	else {
+		cout << "Тип КПП не распознан.\n";
+	}
 }
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+
 	char req;
 	int n;
 
 	Car car_1(2006, 300000, 71568, "Lada", generateVIN(), "Индиго", "Седан", "Механическая");
 	Car car_2(2019, 2250000, 57975, "Mazda", generateVIN(), "Серый", "Хэтчбек", "Автомат");
-	car_1.show();
+	Car car_3(2011, 1685000, 382000, "Audi", generateVIN(), "Чёрный", "Седан", "Автомат");
+
+	cout << "\nАвтомобиль №1:";
+	cout << "\n------------------------------\n";
+	cout << "            ИНФО\n";
+	cout << "Бренд: " << car_1.getBrand() << "\n";	
+	cout << "VIN: " << car_1.getVIN() << "\n";
+	cout << "Год: " << car_1.getYear() << "\n";
+	cout << "Цвет: " << car_1.getColor() << "\n";
+	cout << "Тип кузова: " << car_1.getBodyType() << "\n";
+	cout << "Коробка: " << car_1.getGearbox() << "\n";
+	cout << "Пробег: " << car_1.getMileage() << " км\n";
+	cout << "Цена: " << car_1.getPrice() << " рублей\n";
+	cout << "------------------------------\n\n";
+	cout << "Метод для добавления пробега.\n"; 
 	cout << "Введите пробег, который хотите добавить: ";
 	cin >> n;
 	car_1.addMileage(n);
+	cout << "\n";
+	cout << "Метод для расчёта цены автомобиля по пробегу.\n";
 	car_1.calculatePriceBasedOnMileage();
-
+	cout << "\n";
+	cout << "Метод для расчёта цены автомобиля по типу КПП.\n";
+	car_1.calculatePriceBasedOnGearbox();
+	cout << "\n";
+	cout << "Метод изменения цены.\n";
 	cout << "Желаете ли сменить цену на свою?\n";
 	cout << "Для подтверждения введите « y », в противном случае любой символ.\n";
 	cin >> req;
@@ -129,12 +177,32 @@ int main() {
 		cin >> n;
 		car_1.changePrice(n);
 	}
-	car_2.show();
+	cout << "\n";
+	
+	cout << "\nАвтомобиль №2:";
+	cout << "\n------------------------------\n";
+	cout << "            ИНФО\n";
+	cout << "Бренд: " << car_2.getBrand() << "\n";
+	cout << "VIN: " << car_2.getVIN() << "\n";
+	cout << "Год: " << car_2.getYear() << "\n";
+	cout << "Цвет: " << car_2.getColor() << "\n";
+	cout << "Тип кузова: " << car_2.getBodyType() << "\n";
+	cout << "Коробка: " << car_2.getGearbox() << "\n";
+	cout << "Пробег: " << car_2.getMileage() << " км\n";
+	cout << "Цена: " << car_2.getPrice() << " рублей\n";
+	cout << "------------------------------\n\n";
+	cout << "Метод для добавления пробега.\n";
 	cout << "Введите пробег, который хотите добавить: ";
 	cin >> n;
 	car_2.addMileage(n);
+	cout << "\n";
+	cout << "Метод для расчёта цены автомобиля по пробегу.\n";
 	car_2.calculatePriceBasedOnMileage();
-
+	cout << "\n";
+	cout << "Метод для расчёта цены автомобиля по типу КПП.\n";
+	car_2.calculatePriceBasedOnGearbox();
+	cout << "\n";
+	cout << "Метод изменения цены.\n";
 	cout << "Желаете ли сменить цену на свою?\n";
 	cout << "Для подтверждения введите « y », в противном случае любой символ.\n";
 	cin >> req;
@@ -143,5 +211,40 @@ int main() {
 		cin >> n;
 		car_2.changePrice(n);
 	}
+	cout << "\n";
+
+	cout << "\nАвтомобиль №3:";
+	cout << "\n------------------------------\n";
+	cout << "            ИНФО\n";
+	cout << "Бренд: " << car_3.getBrand() << "\n";
+	cout << "VIN: " << car_3.getVIN() << "\n";
+	cout << "Год: " << car_3.getYear() << "\n";
+	cout << "Цвет: " << car_3.getColor() << "\n";
+	cout << "Тип кузова: " << car_3.getBodyType() << "\n";
+	cout << "Коробка: " << car_3.getGearbox() << "\n";
+	cout << "Пробег: " << car_3.getMileage() << " км\n";
+	cout << "Цена: " << car_3.getPrice() << " рублей\n";
+	cout << "------------------------------\n\n";
+	cout << "Метод для добавления пробега.\n";
+	cout << "Введите пробег, который хотите добавить: ";
+	cin >> n;
+	car_3.addMileage(n);
+	cout << "\n";
+	cout << "Метод для расчёта цены автомобиля по пробегу.\n";
+	car_3.calculatePriceBasedOnMileage();
+	cout << "\n";
+	cout << "Метод для расчёта цены автомобиля по типу КПП.\n";
+	car_3.calculatePriceBasedOnGearbox();
+	cout << "\n";
+	cout << "Метод изменения цены.\n";
+	cout << "Желаете ли сменить цену на свою?\n";
+	cout << "Для подтверждения введите « y », в противном случае любой символ.\n";
+	cin >> req;
+	if (req == 'y') {
+		cout << "Введите новую цену: ";
+		cin >> n;
+		car_3.changePrice(n);
+	}
+	cout << "\n";
 	return 0;
 }
