f9e400aafbfebb354b5d9403b70cdeebff89a558
Erangez <112576544+Erangez@users.noreply.github.com>
2025-05-08 00:01:04 +0300

    Лабораторная работа №3. Классы в C++
    • Фикс метода для изменения цены с учётом пробега
    • Оптимизация кода

--- Lab No. 3 class.cpp
+++ Lab No. 3 class.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 string generateVIN() {
 	srand(time(NULL));
+
 	char symbols[33]{ '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','J','K','L','M','N','P','R','S','T','U','V','W','X','Y','Z' };
 	string VIN = "";
 	for (int i = 0; i < 17; i++) {
@@ -26,35 +27,29 @@ public:
 	Car(int year_of_release, int price, int mileage, string brand, string vin, string color, string body_type, string gearbox);
 	~Car();
 	// Методы
-	void show();
+	void show() const;
 	void addMileage(int km);
 	void changePrice(int new_price);
 	void calculatePriceBasedOnMileage();
 	// Методы доступа к закрытым данным-членам
-	int getYear() { return year_of_release; }
-	int getPrice() { return price; }
-	int getMileage() { return mileage; }
-	string getBrand() { return brand; }
-	string getVIN() { return vin; }
-	string getColor() { return color; }
-	string getBodyType() { return body_type; }
-	string getGearbox() { return gearbox; }
+	int getYear() const { return year_of_release; } // const после метода = константный метод (int const ...) = константное возвращ. значение
+	int getPrice() const { return price; }
+	int getMileage() const { return mileage; }
+	string getBrand() const { return brand; }
+	string getVIN() const { return vin; }
+	string getColor() const { return color; }
+	string getBodyType() const { return body_type; }
+	string getGearbox()const { return gearbox; }
 };
-Car::Car(int year_of_release, int price, int mileage, string brand, string vin, string color, string body_type, string gearbox) {
-	this->vin = vin;
-	this->year_of_release = year_of_release;
-	this->price = price;
-	this->mileage = mileage;
-	this->brand = brand;
-	this->color = color;
-	this->body_type = body_type;
-	this->gearbox = gearbox;
+Car::Car(int year_of_release, int price, int mileage, string brand, string vin, string color, string body_type, string gearbox) :
+	year_of_release{ year_of_release }, price{ price }, mileage{ mileage }, brand{ brand }, vin{ vin }, color{ color }, body_type{ body_type }, gearbox{ gearbox }
+{
 	cout << this->brand << " " << "[" << this->vin << "] " << this->year_of_release << " года была привезена в салон.\n";
 }
 Car::~Car() {
 	cout << this->brand << " " << "[" << this->vin << "] " << this->year_of_release << " года была куплена за " << price << " рублей.\n";
 }
-void Car::show() {
+void Car::show() const {
 	cout << "\n------------------------------\n";
 	cout << "            ИНФО\n";
 	cout << "Бренд: " << brand << "\n";	
@@ -81,7 +76,7 @@ void Car::addMileage(int km) {
 void Car::changePrice(int new_price) {
 	if (new_price != price) {
 		price = new_price;
-		cout << "Для " << brand << " " << "[" << vin << "] " << year_of_release << " года была изменена цена.\n";
+		cout << "Для " << getBrand() << " " << "[" << vin << "] " << year_of_release << " года была изменена цена.\n";
 		cout << "Новая цена: " << price << "рублей.\n";
 	}
 	else {
@@ -92,14 +87,25 @@ void Car::changePrice(int new_price) {
 void Car::calculatePriceBasedOnMileage() {
 	const int fixed_penalty = 18000;
 	int penalty = (mileage / 10000) * fixed_penalty;
-	int penalted_price = price - penalty;
-	if (penalted_price < price / 2) {
+	int penalted_price = price;
+	if (price - penalty < price / 2) {
 		penalted_price /= 2;
+		cout << "Рассматриваемый автомобиль: " << brand << " " << "[" << vin << "] " << year_of_release << " года с пробегом " << mileage << " км\n";
+		cout << "Текущая цена: " << price << " рублей\n";
+		cout << "Вычет составил лишь половину от стоимости автомобиля (" << price / 2 << " рублей) из-за слишком большого пробега.\n";
+		cout << "Расчитанная цена: " << penalted_price << " рублей.\n";
+		price /= 2;
 	}
-	cout << "Рассматриваемый автомобиль: " << brand << " " << "[" << vin << "] " << year_of_release << " года с пробегом " << mileage << " км\n";
-	cout << "Текущая цена: " << price << " рублей\n";
-	cout << "Вычет: -" << penalty << " рублей (с учётом пробега)\n";
-	cout << "Расчитанная цена: " << penalted_price << " рублей.\n";
+	else {
+		penalted_price = price - penalty;
+		cout << "Рассматриваемый автомобиль: " << brand << " " << "[" << vin << "] " << year_of_release << " года с пробегом " << mileage << " км\n";
+		cout << "Текущая цена: " << price << " рублей\n";
+		cout << "Вычет: -" << penalty << " рублей (с учётом пробега).\n";
+		cout << "Расчитанная цена: " << penalted_price << " рублей.\n";
+		price -= penalty;
+	}
+
+	
 }
 int main() {
 	SetConsoleCP(1251);
@@ -115,7 +121,7 @@ int main() {
 	car_1.addMileage(n);
 	car_1.calculatePriceBasedOnMileage();
 
-	cout << "Желаете сменить цену?\n";
+	cout << "Желаете ли сменить цену на свою?\n";
 	cout << "Для подтверждения введите « y », в противном случае любой символ.\n";
 	cin >> req;
 	if (req == 'y') {
@@ -129,11 +135,10 @@ int main() {
 	car_2.addMileage(n);
 	car_2.calculatePriceBasedOnMileage();
 
-	cout << "Желаете сменить цену?\n";
+	cout << "Желаете ли сменить цену на свою?\n";
 	cout << "Для подтверждения введите « y », в противном случае любой символ.\n";
 	cin >> req;
 	if (req == 'y') {
-		int new_price;
 		cout << "Введите новую цену: ";
 		cin >> n;
 		car_2.changePrice(n);
